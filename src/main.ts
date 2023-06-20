973a8ee99e4eb62a51048557baccad397a501336
Jimy <jimyhoulbrook@gmail.com>
2023-06-20 16:36:39 +0100

    Settings finished
    Settings can now be saved to external JSON using button, started layout and design for calculator page

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-// Load data from JSON
-import * as data from './data/storage.json';
+import * as fs from 'fs';
+
+// Type for saved data
+type Data = {
+    hoursWorked: number,
+    money: number
+}
 
 // Function wrapper that runs once DOM content is loaded
 const onReady = () : void => {
-    console.log(typeof(data))
+    // Load daa from JSON
+    const data: Data = require('../data/storage.json');
 
     // Show correct tab when first connecting to page
     const url : string = window.location.href;
@@ -20,17 +26,13 @@ const onReady = () : void => {
     const calcButton = <HTMLElement> document.getElementById("calcButton");
     const confButton = <HTMLElement> document.getElementById("confButton");
 
-    calcButton.addEventListener('click', () : void =>{
-        showCalc(calc, conf);
-    });
-    confButton.addEventListener('click', () : void => {
-        showConf(calc, conf);
-    });
+    calcButton.addEventListener('click', () : void => showCalc(calc, conf));
+    confButton.addEventListener('click', () : void => showConf(calc, conf));
 
     // Listener for save button
     const saveButton = <HTMLElement> document.getElementById("saveButton");
 
-    saveButton.addEventListener('click', saveInfo)
+    saveButton.addEventListener('click', () : void => saveInfo(data));
 }
 
 const showCalc = (calc : HTMLElement, conf : HTMLElement) : void => {
@@ -43,8 +45,43 @@ const showConf = (calc : HTMLElement, conf : HTMLElement) : void => {
     conf.style.display = "flex";
 }
 
-const saveInfo = () : void => {
+const saveInfo = (data : Data) : void => {
+    // Get HTML elements
+    const allHoursIpt = <HTMLInputElement> document.getElementById("allHoursInput");
+    const moneyIpt = <HTMLInputElement> document.getElementById("moneyInput");
+    const displaytoUser = <HTMLElement> document.getElementById("displayToUser");
 
+    // Check that both inputs have been used
+    const allHours : number = parseFloat(allHoursIpt.value);
+    const money : number = parseFloat(moneyIpt.value);
+
+    if(isNaN(allHours) || isNaN(money)){
+        displaytoUser.innerHTML = "Please enter a value to both fields";
+        return;
+    }
+
+    // Update Data object
+    data.hoursWorked = allHours;
+    data.money = money;
+
+    // Update storage.json
+    const toWrite : string = JSON.stringify(data);
+
+    fs.writeFile('./data/storage.json', toWrite, (e) => {
+        if(e){
+            console.error(e);
+
+            displaytoUser.textContent = "";
+            displaytoUser.innerHTML = "Error saving settings"
+
+            throw e;
+        }
+        
+        displaytoUser.textContent = "";
+        displaytoUser.style.color = "green";
+        displaytoUser.innerHTML = "Settings Saved"
+    });
 }
 
-window.addEventListener('DOMContentLoaded', onReady)
+
+window.addEventListener('DOMContentLoaded', onReady);
